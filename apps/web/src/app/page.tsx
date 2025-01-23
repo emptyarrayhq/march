cb71fa12fb7372c5d9f3b0816f8207adae0859d1
sajdkabir <sajda.kbir@gmail.com>
2025-01-23 16:39:27 +0530

    feat: init the web

---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2 text-lg">
+     <h1 className="text-3xl"> Welcome !!!</h1>
+    </div>
+  );
+}
